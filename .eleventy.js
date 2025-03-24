deb9e976524855e63ca9487dc6d48f215e497938
christopher-echols <76822699+christopher-echols@users.noreply.github.com>
2025-03-24 15:37:56 -0400

    Add Markdown parser, update eleventy.js config, create sample .md page

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,21 @@
 module.exports = function(eleventyConfig) {
 
-  // Explicit configuration
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
   eleventyConfig.addPassthroughCopy({ "src/js": "js" });
   eleventyConfig.addPassthroughCopy({ "src/img": "img" });
   eleventyConfig.addPassthroughCopy({ "src/assets": "/assets" });
- 
-  // Add CNAME file to be copied to the output
   eleventyConfig.addPassthroughCopy({ "src/CNAME": "CNAME" });
+  
+  // Set Markdown library options
+  let markdownIt = require("markdown-it");
+  let markdownItOptions = {
+    html: true,        // Enable HTML tags in source
+    breaks: true,      // Convert '\n' in paragraphs into <br>
+    linkify: true      // Autoconvert URL-like text to links
+  };
+  
+  // Configure the markdown library
+  eleventyConfig.setLibrary("md", markdownIt(markdownItOptions));
   
   return {
     dir: {
---
+++ src/about-md.md
@@ -0,0 +1,21 @@
+---
+layout: base.liquid
+title: About
+description: About
+featureImage: /assets/about/echols-self-portrait.png
+featureImageAlt: A sketched self portrait of Christopher Echols
+featureImageStyle: "width: 40%;"
+subtitle: An autobiography in five sentences.
+---
+
+I love making things and telling stories.
+
+As a kid, I thought I'd grow up to be an illustrator or a wildlife photographer. Instead, I ended up making television shows and building websites for a living.
+
+My wonderful wife and I have two utterly stupendous kids.
+
+In my spare time, I draw birds.
+
+---
+
+<small>This website is built the old fashioned way, using nothing but HTML and CSS. Like any handmade thing, it is imperfect. Please <a href="mailto:echols@hey.com">let me know</a> if you find something broken so I can repair it.</small>
